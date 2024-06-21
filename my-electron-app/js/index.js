8e3fc002c19c27505faef7605530347b18d55981
Naxocist <subforwork@gmail.com>
2024-06-22 02:37:35 +0700

    Add functionality to openseadragon

--- my-electron-app/js/index.js
+++ my-electron-app/js/index.js
@@ -8,49 +8,29 @@ var viewer =  OpenSeadragon({
     ajaxWithCredentials: true,
     sequenceMode: true,
     animationTime: 0.5,
-    zoomPerScroll: 1.7
+    zoomPerScroll: 1.7,
+
+    showNavigator: true,
+    navigatorPosition: "BOTTOM_RIGHT", 
+    navigatorSizeRatio: 0.6,
+
+    visibilityRatio: 0.8,
+    constrainDuringPan: true,
+
+    minZoomLevel: 0.7,
+    defaultZoomLevel:	0.7,
   });
 
 const fileInput = document.getElementById('fileInput');
 
-fileInput.addEventListener('change', function() {
+fileInput.addEventListener('change', async () => {
   const file = fileInput.files[0];
   console.log('Selected file:', file);
   viewer.close();
 
-  let tiffTileSources = OpenSeadragon.GeoTIFFTileSource.getAllTileSources(file, {
+  let tf = await OpenSeadragon.GeoTIFFTileSource.getAllTileSources(file, {
     logLatency: false,
   });
 
-  tiffTileSources.then( tf => viewer.open(tf))
+  viewer.open(tf)
 });
-
-
-// const view = async () => {
-
-//     const path = 'D:/NSC2024_dataset/svs/S58-03668 B.svs'
-
-//     let tiffTileSources = await OpenSeadragon.GeoTIFFTileSource.getAllTileSources(path, {
-//       logLatency: true,
-//     });
-
-//     var test = OpenSeadragon.Viewer({
-//         id: 'dz',
-//         crossOriginPolicy: "Anonymous",
-//         ajaxWithCredentials: true,
-//         prefixUrl: "../openseadragon/images/",
-//         tileSources: tiffTileSources,
-//         // animationTime: 0,
-//         // zoomPerScroll: 1.7
-//     });
-
-//     // const viewer = new OpenSeadragon.Viewer({
-//     //   id: 'dz',
-//     //   crossOriginPolicy: "Anonymous",
-//     //   ajaxWithCredentials: true,
-//     //   tileSources: tiffTileSources,
-//     // });
-// }
-
-// // view()
-
